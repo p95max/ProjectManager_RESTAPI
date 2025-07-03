ba014f1c6cf4efd31c22363886c5717623cdf37b
maxx <maxpetrikin@gmail.com>
2025-07-03 23:47:10 +0200

    prepare for deploy

--- ProjectManager/settings.py
+++ ProjectManager/settings.py
@@ -119,11 +119,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-# for PRODUCTION!!
-
-# CORS_ALLOWED_ORIGINS = [
-#     "https://your-frontend-domain.com",
-#     "http://localhost:3000",
-# ]
+CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
+CORS_ALLOWED_ORIGINS = [
+    origin.strip() for origin in os.getenv('CORS_ALLOWED_ORIGINS', '').split(',') if origin.strip()
+]
