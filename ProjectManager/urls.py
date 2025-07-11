6cbd6f6719cfa51ba5cf37decc79d325f0ff6576
maxx <maxpetrikin@gmail.com>
2025-07-11 09:50:09 +0200

    swagger configured

--- ProjectManager/urls.py
+++ ProjectManager/urls.py
@@ -12,8 +12,6 @@ from rest_framework_simplejwt.views import (
 )
 
 
-from drf_yasg import openapi
-
 schema_view = get_schema_view(
     openapi.Info(
         title="Project Manager API",
@@ -26,7 +24,6 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
     authentication_classes=[],
-    security=[{'Bearer': []}],
 )
 
 
